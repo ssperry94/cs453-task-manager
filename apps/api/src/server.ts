8a9ecb658d081e6e7b1366ce55c80c5c5a3a3385
Sam Perry <s.sperry94@gmail.com>
2026-07-03 17:12:07 -0500

    Added envronment reading and basic start of server.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,10 +1,30 @@
-import { type Task, Status } from "./types.js"
+import { type Task, Status } from "./types.js";
+import express, { type Application, type Request, type Response } from 'express';
+import 'dotenv/config';
 
+// Read in environment variables
+const PORT = Number(process.env.PORT ?? 3000);
+const dbUrl = process.env.DATABASE_URL || "";
+const jwToken = process.env.JWT_SECRET || "";
+
+
+// TODO(#2) Move to database
 const tasks: Task[] = [
     {id: 1, title: "Dummy Task 1", status: Status.IN_PROGRESS},
     {id: 2, title: "Dummy Task 2", status: Status.TODO}
 ];
 
-for (const task of tasks) {
-    console.log(JSON.stringify(task));
+const app = express();
+app.use(express.json());
+
+async function main() {
+    //TODO(#2): Add db connection logic
+    app.listen(PORT, () => {
+        console.log(`Connected to server on port: ${PORT}`);
+    });
 }
+
+main().catch((err) => {
+    console.error("Error starting server: ", err);
+    process.exit(1);
+});
