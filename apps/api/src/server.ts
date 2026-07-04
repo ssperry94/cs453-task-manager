a19cab916b2aaa5c40598761f5410737c69b8921
Sam Perry <s.sperry94@gmail.com>
2026-07-04 16:03:02 -0500

    Added schema's for validation, added post method.

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,4 +1,4 @@
-import { type Task, Status } from "./types.js";
+import { TaskCreationSchema, Task, TaskCreation, Status, HttpStatus, TaskSchema } from "./types.js";
 import express, { type Application, type Request, type Response } from 'express';
 import 'dotenv/config';
 
@@ -7,7 +7,8 @@ const PORT = Number(process.env.PORT ?? 3000);
 const dbUrl = process.env.DATABASE_URL || "";
 const jwToken = process.env.JWT_SECRET || "";
 
-
+// TODO(#2): Database needs to handle ID
+let currentId = 2;
 // TODO(#2) Move to database
 const tasks: Task[] = [
     {id: 1, title: "Dummy Task 1", status: Status.IN_PROGRESS},
@@ -16,6 +17,31 @@ const tasks: Task[] = [
 
 const app = express();
 app.use(express.json());
+
+/**
+ * The /tasks get request
+ */
+app.get("/tasks", (req: Request, res: Response) => {
+    return res.status(HttpStatus.OK).json(tasks);
+});
+
+app.post("/tasks", (req: Request, res: Response) => {
+    const result = TaskCreationSchema.safeParse(req.body);
+    if (!result.success) {
+        return res.status(HttpStatus.BAD_REQUEST).json({errors: result.error.issues});
+    }
+
+    const newTask = {
+        id: ++currentId,
+        title: req.body.title,
+        status: req.body.status
+    }
+
+    // Add the new task
+    tasks.push(newTask);
+
+    return res.status(HttpStatus.OK).json(newTask);
+});
 
 async function main() {
     //TODO(#2): Add db connection logic
--- apps/api/src/types.ts
+++ apps/api/src/types.ts
@@ -2,6 +2,8 @@
  * Holds the object representation of a task
 */
 
+import * as z from 'zod';
+
 export const Status = {
     TODO: "todo",
     DONE: "done",
@@ -11,8 +13,40 @@ export const Status = {
 
 export type Status = typeof Status[keyof typeof Status];
 
-export interface Task {
-    id: number,
-    title: string,
-    status: Status
-};
+// Schema's
+export const StatusSchema = z.enum([
+    Status.TODO,
+    Status.DONE,
+    Status.IN_PROGRESS,
+    Status.WONT_DO
+]);
+
+export const TaskSchema = z.object({
+    id: z.number(),
+    title: z.string(),
+    status: StatusSchema
+});
+
+export const TaskCreationSchema = TaskSchema.omit({id: true});
+
+// Infer types from Zod Schema's
+export type Task = z.infer<typeof TaskSchema>;
+export type TaskCreation = z.infer<typeof TaskCreationSchema>;
+
+// export interface Task {
+//     id: number,
+//     title: string,
+//     status: Status
+// };
+
+export const HttpStatus = {
+    OK: 200,
+    CREATED: 201,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export type HttpStatus = typeof HttpStatus[keyof typeof HttpStatus];
