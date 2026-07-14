d28920d331c6a8c7a4dc226a7f58402f9b55aafe
Sam Perry <s.sperry94@gmail.com>
2026-07-13 21:26:19 -0500

    Created first iterations of rest of routes.

--- apps/api/src/middleware/validation.ts
+++ apps/api/src/middleware/validation.ts
@@ -1,11 +1,31 @@
-import { TaskCreationSchema, HttpStatus } from "../types.js";
+import { TaskCreationSchema, TaskUpdateSchema, HttpStatus, IdSchema } from "../types.js";
 import { type Request, type Response, type NextFunction } from 'express';
 
 export function validateTask(req: Request, res: Response, next: NextFunction) {
     // Verify the incoming task has all correct types and fields
     const result = TaskCreationSchema.safeParse(req.body);
     if (!result.success) {
-        return res.status(HttpStatus.BAD_REQUEST).json({errors: result.error.issues});
+        return res.status(HttpStatus.BAD_REQUEST).json(result.error.issues);
+    }
+
+    next();
+}
+
+export function validateId(req: Request, res: Response, next: NextFunction) {
+    const result = IdSchema.safeParse(req.params);
+
+    if (!result.success) {
+        return res.status(HttpStatus.BAD_REQUEST).json(result.error.issues);
+    }
+
+    next();
+}
+
+export function validateTaskUpdate(req: Request, res: Response, next: NextFunction) {
+    const result = TaskUpdateSchema.safeParse(req.body);
+
+    if (!result.success) {
+        return res.status(HttpStatus.BAD_REQUEST).json(result.error.issues);
     }
 
     next();
--- apps/api/src/routes/tasks.ts
+++ apps/api/src/routes/tasks.ts
@@ -4,7 +4,7 @@
 
 import { type Task, Status, HttpStatus } from "../types.js";
 import  Router, { type Request, type Response } from 'express';
-import { validateTask } from '../middleware/validation.js';
+import { validateTask, validateTaskUpdate, validateId } from '../middleware/validation.js';
 
 // TODO(#3): Database needs to handle ID
 let currentId = 2;
@@ -20,14 +20,28 @@ export const taskRouter = Router();
 /**
  * The /tasks get request. Returns all known tasks
  */
-taskRouter.get("/tasks", (req: Request, res: Response) => {
+taskRouter.get("/tasks", async (req: Request, res: Response) => {
     return res.status(HttpStatus.OK).json(tasks);
 });
 
 /**
+ * The /tasks/:id get request. Returns a specific task if found, otherwise 404
+ */
+taskRouter.get("/tasks/:id", validateId, async (req: Request, res: Response) => {
+    const result = tasks.find((task) => {return task.id === Number(req.params.id)});
+
+    if (result === undefined) {
+        return res.status(404).json({error : `Task with id ${req.params.id} was not found.`});
+    }
+
+    return res.status(200).json(result);
+});
+
+
+/**
  * The /tasks post request. Adds a new task to the task database and sets the ID
  */
-taskRouter.post("/tasks", validateTask, (req: Request, res: Response) => {
+taskRouter.post("/tasks", validateTask, async (req: Request, res: Response) => {
     const newTask = {
         id: ++currentId,
         title: req.body.title,
@@ -37,4 +51,42 @@ taskRouter.post("/tasks", validateTask, (req: Request, res: Response) => {
     // Add the new task
     tasks.push(newTask);
     return res.status(HttpStatus.CREATED).json(newTask);
+});
+ 
+/**
+ * The /tasks/:id PATCH request. Updates the send fields of the task. Returns 404 if not found. At least one required field must be present
+ */
+taskRouter.patch("/tasks/:id", validateId, validateTaskUpdate, async (req: Request, res: Response) => {
+   const task = tasks.find(task => task.id === Number(req.params.id));
+
+    if (task === undefined) {
+        return res.status(404).json({error : `Task with the id ${req.params.id} was not found.`});
+    }
+
+    if (req.body.title !== undefined) {
+        task.title = req.body.title;
+    }
+
+    if (req.body.status !== undefined) {
+        task.status = req.body.status;
+    }
+
+    return res.status(200).json(task);
+});
+
+/**
+ * Deletes a task. Returns 404 if the task is not found
+ */
+taskRouter.delete("/tasks/:id", validateId, async (req: Request, res: Response) => {
+    const index = tasks.findIndex(task => task.id === Number(req.params.id));
+
+    if (index < 0) {
+      return res.status(404).json({
+        error: `Task with id ${req.params.id} could not be found.`
+      });
+    }
+
+    tasks.splice(index, 1);
+
+    return res.sendStatus(HttpStatus.NO_CONTENT);
 });
